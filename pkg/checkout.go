20b3215a1f7330c7f0f41c816e16a2e880636e0b
Sophie <sophie.cooper@7digital.com>
2020-05-04 14:46:27 +0100

    Extract discount figures

--- pkg/checkout.go
+++ pkg/checkout.go
@@ -7,6 +7,15 @@ var itemValues = map[rune] int {
 	'D': 15,
 }
 
+type itemDiscount struct {
+	AmountNeeded, Discount int
+}
+
+var discounts = map[rune] itemDiscount {
+	'A': itemDiscount {3, 20},
+	'B': itemDiscount {2, 15},
+}
+
 func checkout(items string) int {
 	var itemCounts = map[rune] int {}
 
@@ -18,8 +27,8 @@ func checkout(items string) int {
 		itemCounts[item] = itemCounts[item] + 1
 	}
 
-	total -= (itemCounts['A'] / 3) * 20
-	total -= (itemCounts['B'] / 2) * 15
+	total -= (itemCounts['A'] / discounts['A'].AmountNeeded) * discounts['A'].Discount
+	total -= (itemCounts['B'] / discounts['B'].AmountNeeded) * discounts['B'].Discount
 
 	return total
 }
